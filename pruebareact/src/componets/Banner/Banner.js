621a7e6327785ddd5d90639d0820850d7a787307
cesarrodriguez26 <124197857+cesarrodriguez26@users.noreply.github.com>
2023-05-29 20:48:01 +0200

    primera preentrega

---
+++ pruebareact/src/componets/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+  <div className="banner">
+    <div className="banner-container">
+        <h1>Compra sin Complicaciones</h1>
+        <p>Los mejores Productos a tu Mesa</p>
+    </div>
+  
+  </div>
+  )
+};
+
+export default Banner;

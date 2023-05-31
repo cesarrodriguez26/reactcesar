1ab549b2944a8d857bbcee64bed1bf432fe6e5c5
cesarrodriguez26 <124197857+cesarrodriguez26@users.noreply.github.com>
2023-05-31 19:25:13 +0200

    primerapreentregareact

--- pruebareact/src/componets/Home/Home.js
+++ pruebareact/src/componets/Home/Home.js
@@ -1,10 +1,12 @@
 import Navbar from "../Navbar/Navbar";
 import Banner from "../Banner/Banner";
+import Products from "../Products/Products";
 const Home = () => {
     return (
         <>
     <Navbar />
     <Banner />
+    <Products />
     </>
     );
 };
